ff816449c9bd965bb059147c529bfb4c94aaa3c6
VivekKatre1582003 <vivekradheshyam158@gmail.com>
2025-05-30 09:36:26 +0530

    Application file

---
+++ src/main/java/com/Vivek/Rentmate/RentmateApplication.java
@@ -0,0 +1,13 @@
+package com.Vivek.Rentmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentmateApplication.class, args);
+	}
+
+}
